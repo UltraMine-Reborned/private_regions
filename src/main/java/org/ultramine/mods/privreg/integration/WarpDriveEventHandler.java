b9225d286f071d52de36c4a08c3a1460df53b217
Arseny Spiridonov <sirse.box@gmail.com>
2018-07-03 18:01:32 +0700

    WarpDrive support (new WD API)
    Signed-off-by: Arseny Spiridonov <sirse.box@gmail.com>

--- src/main/java/org/ultramine/mods/privreg/integration/WarpDriveEventHandler.java
+++ src/main/java/org/ultramine/mods/privreg/integration/WarpDriveEventHandler.java
@@ -5,36 +5,45 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import cr0s.warpdrive.api.EventWarpDrive;
-import cr0s.warpdrive.block.movement.TileEntityShipCore;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.AxisAlignedBB;
 import org.ultramine.mods.privreg.PrivateRegions;
+import org.ultramine.mods.privreg.modules.RegionModuleBasic;
 import org.ultramine.mods.privreg.regions.Region;
 import org.ultramine.regions.BlockPos;
 import org.ultramine.regions.IRegion;
 import org.ultramine.regions.Rectangle;
 
+import java.util.ArrayList;
 import java.util.Set;
+
+import static org.ultramine.mods.privreg.modules.RegionModuleBasic.RIGHT_BASIC;
 
 @SideOnly(Side.SERVER)
 public class WarpDriveEventHandler {
 
     @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onPreJumpEvent(EventWarpDrive.Ship.PreJump preJumpEvent) {
-        World world = preJumpEvent.worldCurrent;
-        TileEntity tileEntity = world.getTileEntity(preJumpEvent.xCurrent, preJumpEvent.yCurrent, preJumpEvent.zCurrent);
+    public void onTargetCheckEvent(EventWarpDrive.Ship.TargetCheck targetCheck) {
+        AxisAlignedBB targetAaBb = targetCheck.aabbTarget;
+        Rectangle targetRect = new Rectangle(new BlockPos(targetAaBb.minX, targetAaBb.minY, targetAaBb.minZ), new BlockPos(targetAaBb.maxX, targetAaBb.maxY, targetAaBb.maxZ));
+        Set<IRegion> regions = PrivateRegions.instance().getServerRegionManager(targetCheck.worldTarget.provider.dimensionId).getRegionsInRange(targetRect);
 
-        if(tileEntity instanceof TileEntityShipCore) {
-            TileEntityShipCore shipCore = (TileEntityShipCore) tileEntity;
-            Rectangle warpBox = new Rectangle(new BlockPos(shipCore.minX, shipCore.minY, shipCore.minZ), new BlockPos(shipCore.maxX, shipCore.maxY, shipCore.maxZ));
-            Set<IRegion> regions = PrivateRegions.instance().getServerRegionManager(world.provider.dimensionId).getRegionsInRange(warpBox);
-
-            if(!regions.isEmpty()) {
-                for (IRegion iRegion : regions) {
-                    Region region = (Region) iRegion;
-                    if(region.isActive()) {
-                        preJumpEvent.appendReason("You are don't have permissions to warp into protected region!");
-                        preJumpEvent.setCanceled(true);
+        if (!regions.isEmpty()) {
+            for (IRegion iRegion : regions) {
+                Region region = (Region) iRegion;
+                if (region.isActive() && region.hasModule(RegionModuleBasic.class)) {
+                    ArrayList<String> players = (ArrayList<String>) targetCheck.shipController.getAttachedPlayers()[1];
+                    if(!players.isEmpty()) {
+                        for(String playerName : players) {
+                            EntityPlayer entityPlayer = MinecraftServer.getServer().getConfigurationManager().getPlayerByUsername(playerName);
+                            if(entityPlayer != null) {
+                                if (!region.hasRight(entityPlayer.getGameProfile(), RIGHT_BASIC)) {
+                                    targetCheck.appendReason("One of the players on the ship don't have permission to private region");
+                                    targetCheck.setCanceled(true);
+                                }
+                            }
+                        }
                     }
                 }
             }
